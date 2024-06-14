fc64c1c2454549109bffe4e7f6a0eb2f063e8917
Cheree <cheree.lanece@gmail.com>
2024-06-14 10:55:12 -0400

    added navigation, footer links, and logo

--- src/app/components/Navigation/NavBar.tsx
+++ src/app/components/Navigation/NavBar.tsx
@@ -1,8 +1,16 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Logo from '../../../../public/Strategi IQ transparent (2).svg'
 const NavBar = () => {
     return (
-        <nav>
-            <Link href="../../pages/blog"></Link>
+        <nav className={`w-full h-6 flex justify-center items-center pt-8 pb-8`}>
+            <div className={`logoContainer w-fit `}>
+                <Image src={Logo} alt="logo image" width={100} height={100}/>
+            </div>
+            <div className="navLinks w-full flex justify-center items-center space-x-4">
+                <Link href="/" className={`link`}>Map</Link>
+                <Link href="../../pages/blog" className={`link`}>Blog</Link>
+            </div>
         </nav>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,20 @@ import NavBar from "@/app/components/Navigation/NavBar";
 import Iframe from 'react-iframe'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8">
+    <main className="flex min-h-screen flex-col items-center justify-between pl-8 pr-8">
         <NavBar/>
-        <div className={`electionMap bg-slate-400 w-full h-96`}>
-            <Iframe url={"strategi_map.html"} className={`w-full h-full`}/>
+        <div className={`electionMap bg-slate-400 w-full min-h-screen`}>
+            <Iframe url={"strategi_map.html"} className={`w-full min-h-screen`}/>
         </div>
-        <div className={`articleContainer w-full h-96 flex flex-wrap justify-around`}>
-            <div className="article w-96 h-80 bg-slate-400 "></div>
-            <div className="article w-96 h-80 bg-slate-400"></div>
-            <div className="article w-96 h-80 bg-slate-400"></div>
+        <div className="otherLinks w-full flex justify-center items-center space-x-4 pt-8 pb-8">
+            <Link href="/" className={`link`}>About Us</Link>
+            <Link href="/" className={`link`}>Contact Us</Link>
         </div>
+        {/*<div className={`articleContainer w-full h-96 flex flex-wrap justify-around`}>*/}
+        {/*    <div className="article w-96 h-80 bg-slate-400 "></div>*/}
+        {/*    <div className="article w-96 h-80 bg-slate-400"></div>*/}
+        {/*    <div className="article w-96 h-80 bg-slate-400"></div>*/}
+        {/*</div>*/}
     </main>
   );
 }
